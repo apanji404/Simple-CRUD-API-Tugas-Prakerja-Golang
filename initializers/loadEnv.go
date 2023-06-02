19c242c342d9d5393e1b2eeed651ae46f4f5933a
Agung <apanji133@gmail.com>
2023-06-02 18:46:33 +0700

    remove loadEnv

--- initializers/loadEnv.go
+++ initializers/loadEnv.go
@@ -1,14 +1,8 @@
 package initializers
 
-import (
-	"log"
-
-	"github.com/joho/godotenv"
-)
-
 func LoadEnvVariables() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	//	log.Fatal("Error loading .env file")
+	// }
 }
